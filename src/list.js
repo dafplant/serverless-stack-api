3a4961e58ec0b7696cf68ae4c2c90b2470b564f9
Christopher Plant <chris.daf@gmail.com>
2019-12-19 16:51:13 +0000

    Updated import paths

--- src/list.js
+++ src/list.js
@@ -1,5 +1,5 @@
-import * as dynamoDBLib from "./libs/dynamodb-lib";
-import { success, failure } from "./libs/response-lib";
+import * as dynamoDBLib from "../libs/dynamodb-lib";
+import { success, failure } from "../libs/response-lib";
 
 export async function main(event, context) {
     const params = {
